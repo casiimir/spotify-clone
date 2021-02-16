7f956932390a949121f6cc736b175015e3fde3b6
Casimiro <cp.ciancimino@gmail.com>
2021-02-16 17:17:17 +0100

    edit: queen page added popular tracks and list of all albums

--- js/queen.js
+++ js/queen.js
@@ -1,8 +1,9 @@
-import { getTopTen, getAlbumsFrom, popWrapperList } from './home.js';
+import { getTopTen, getAlbumsFrom, getTracksByAlbum } from './home.js';
 
 // Create Element for the Top Ten Track, necessary to populates track list
-const createTrackItem = (trackNum, trackTitle, trackScore, parentEl) => {
+const popTracksDOM = (trackNum, trackTitle, trackScore, parentEl) => {
   const parent = document.querySelector(parentEl);
+
   const container = document.createElement('li');
   container.dataset.noSelect = true;
 
@@ -19,31 +20,66 @@ const createTrackItem = (trackNum, trackTitle, trackScore, parentEl) => {
   totalPlays.textContent = trackScore;
 
   container.append(num, fav, title, totalPlays);
-  parent.appendChild(container);
+  return parent.appendChild(container);
 }
 
-// Self init
+// Create Element for every single Album in the Artist's obtained data
+const popAlbumsDOM = (albumImg, productYear, albumTitle, idAlbum, parentEl) => {
+  const parent = document.querySelector(parentEl);
 
-// Populates mostPopular__list track's items
+  const mainWrapper = document.createElement('div');
+  mainWrapper.id = idAlbum;
+
+  const container = document.createElement('div');
+  container.classList.add('albumList__piece--header');
+  
+  const image = document.createElement('img');
+  image.src = albumImg;
+
+  const headerInfo = document.createElement('div');
+  headerInfo.classList.add('header__info');
+
+  const paragraph = document.createElement('p');
+  paragraph.textContent = productYear;
+
+  const title = document.createElement('h2');
+  title.textContent = albumTitle;
+
+  const albumList = document.createElement('ul');
+  albumList.classList.add('albumList__piece--list');  
+
+  headerInfo.append(paragraph, title);
+  container.append(image, headerInfo);
+  mainWrapper.append(container, albumList);
+  parent.appendChild(mainWrapper);
+}
+
+// Populates an album with every single track in it
+const popTrackInAlbumDOM = () => {
+
+}
+
+
+
+// Self init
 const topTenData = getTopTen('Queen');
 
 topTenData.then((data) => {
   data.track.forEach((track, i) => {
     const score = Math.floor(track.intScore) + '/10';
 
-    createTrackItem(i, track.strTrack, score, '.mostPopular__list');
+    popTracksDOM(i, track.strTrack, score, '.mostPopular__list');
   });
 })
 
 // get first five albums
 getAlbumsFrom(111238).then((albums) => {
-  console.log(albums.album.slice(0,5));
+  albums.album.forEach((album) => {
+    let albumCover;
+
+    if (album.strAlbumThumb) albumCover = album.strAlbumThumb + '/preview';
+    else albumCover = './default-cover.png';
+
+    popAlbumsDOM(albumCover, album.intYearReleased, album.strAlbum, album.idAlbum, '.albumList__piece');
+  })
 })
-
-
-
-//popWrapperList(111238, 'album', 'i', '.albumList__piece--list');
-
-
-
-// Populates album
