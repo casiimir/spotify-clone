7ec3ccb51a34afba865617702c50c884f4866326
Casimiro <cp.ciancimino@gmail.com>
2021-02-19 14:20:16 +0100

    add: coldplay, bee-gees, the-beatles pages - add: audio folder - edit: queen.js to artistPaje.js

---
+++ js/trackListMP3.js
@@ -0,0 +1,42 @@
+// Start audio on click in the Popular track
+const trackListMP3 = {
+  "queen": [    
+    '../audio/queen/1.mp3',
+    '../audio/queen/2.mp3',
+    '../audio/queen/3.mp3',
+    '../audio/queen/4.mp3',
+    '../audio/queen/5.mp3',
+    '../audio/queen/6.mp3',
+  ],
+  "coldplay": [    
+    '../audio/coldplay/1.mp3',
+    '../audio/coldplay/2.mp3',
+    '../audio/coldplay/3.mp3',
+    '../audio/coldplay/4.mp3',
+    '../audio/coldplay/5.mp3',
+    '../audio/coldplay/6.mp3',
+    '../audio/coldplay/7.mp3',
+    '../audio/coldplay/8.mp3',
+    '../audio/coldplay/9.mp3',
+    '../audio/coldplay/10.mp3',
+  ],
+  "bee+gees": [
+    '../audio/bee-gees/1.mp3',
+    '../audio/bee-gees/2.mp3',
+    '../audio/bee-gees/3.mp3',
+  ],
+  "the+beatles": [
+    '../audio/the-beatles/1.mp3',
+    '../audio/the-beatles/2.mp3',
+    '../audio/the-beatles/3.mp3',
+    '../audio/the-beatles/4.mp3',
+    '../audio/the-beatles/5.mp3',
+    '../audio/the-beatles/6.mp3',
+    '../audio/the-beatles/7.mp3',
+    '../audio/the-beatles/8.mp3',
+    '../audio/the-beatles/9.mp3',
+    '../audio/the-beatles/10.mp3',
+  ]
+}
+
+export { trackListMP3 };
